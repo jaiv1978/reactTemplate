aae5c46eed608f055fc6f7827b204e5ed61f779c
Jorge Arturo Ibarra Valle <jibarra@belatrix.com>
2019-09-25 08:41:56 -0500

    new changes

---
+++ src/containers/templates/components/template-ws01.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+const TemplateWS01 = (props) => {
+    return(
+        <Grid container>
+            <Grid item>
+                
+            </Grid>
+        </Grid>
+    );
+}
+
+export default TemplateWS01;
