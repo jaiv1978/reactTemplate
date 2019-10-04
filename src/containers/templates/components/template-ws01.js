b4d722df0e3ffe0cd8a6b37b9aec98d8ea9480a2
Jorge Arturo Ibarra Valle <jibarra@belatrix.com>
2019-10-04 16:58:29 -0500

    NEW CHABGES

--- src/containers/templates/components/template-ws01.js
+++ src/containers/templates/components/template-ws01.js
@@ -6,11 +6,12 @@ import landscape from '../../../lib/images/landscape.jpg'
 import '../../../styles/containers/templates/components/template-ws01-style.css';
 
 const TemplateWS01 = (props) => {
+    //~
     return(
         <Grid container>
             <Grid xs={12} item><MenuBar /></Grid>
-            <Grid xs={12} style={{ height : "100%" }} item><img src={landscape} alt="landscape" className="imagen" ></img> </Grid>
-        </Grid> 
+            <Grid xs={12} style={{ height : "100%" }} item><img src={require('lib/images/landscape.jpg')} alt="landscape" className="imagen" ></img> </Grid>
+        </Grid>
     );
 }
 
